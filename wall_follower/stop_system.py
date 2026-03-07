df6cc454f155fc22cd57a5701b9c79371a4c6bac
Selinna <selinnalin0705@gmail.com>
2026-03-07 16:10:31 -0500

    new

--- wall_follower/stop_system.py
+++ wall_follower/stop_system.py
@@ -1,3 +1,5 @@
+from math import dist
+
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -8,14 +10,14 @@ class StopSystem(Node):
     def __init__(self):
         super().__init__("stop_system")
         self.declare_parameter("scan_topic", "/scan")
-        self.declare_parameter("drive_topic", "/vesc/low_level/ackermann_cmd")
+        self.declare_parameter("vel_topic", "/vesc/odom")
         self.declare_parameter("stop_topic", "/vesc/low_level/input/safety")
 
         self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
-        self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
+        self.VEL_TOPIC = self.get_parameter('vel_topic').get_parameter_value().string_value
 
         self.safety_margin = 0.2   # minimum buffer
-        self.max_decel = 1.8       # max deceleration
+        self.max_decel = 1.5       # max deceleration
         self.min_dist = float('inf')
 
         self.velocity = 0.0
@@ -28,8 +30,8 @@ class StopSystem(Node):
         )
         self.drive_subscriber = self.create_subscription(
             AckermannDriveStamped,
-            self.DRIVE_TOPIC,
-            self.drive_callback,
+            self.VEL_TOPIC,
+            self.vel_callback,
             10
         )
 
@@ -43,6 +45,7 @@ class StopSystem(Node):
         ranges = np.array(msg.ranges)
         angles = np.arange(len(ranges)) * msg.angle_increment + msg.angle_min
 
+
         # Consider only a frontal cone
         frontal_mask = np.abs(angles) < 0.25
         frontal_ranges = ranges[frontal_mask]
@@ -50,11 +53,20 @@ class StopSystem(Node):
         self.min_dist = np.min(frontal_ranges)
         # turning_distance = 0.5/np.sin(0.34)
 
-        stopping_distance = (self.velocity ** 2) / (2.0 * self.max_decel) + self.safety_margin
+        stop_dist = self.get_stopping_distance(self.velocity)
 
-        if self.min_dist < stopping_distance:
+        stop_dist = 0.
+        if self.min_dist < stop_dist:
             self.publish_stop()
 
+    def get_stopping_distance(self, vel):
+        # Compute stopping distance
+        dist = 0.137 * max(abs(vel), 0.0) ** 2.48 + self.safety_margin
+
+        # Log the value (do NOT call get_stopping_distance again)
+        self._logger.info(f"Current velocity: {vel:.2f} m/s, Stopping distance: {dist:.2f} m, Min distance: {self.min_dist:.2f} m")
+
+        return dist
 
     def drive_callback(self, msg):
         self.velocity = msg.drive.speed
